74cbda1c66d856a66423f7ecef43c8a7971dd897
Ben Manekin <ben.manekin@gmail.com>
2019-02-20 03:54:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,23 @@
 class Dog
+<<<<<<< HEAD
 
   def bark
     puts "Woof!"
   end
 
+=======
+  
+  def bark
+    puts "Woof!"
+  end
+  
+>>>>>>> c3506df4de7da6863fa144b632a6cdb54b2a15b8
   def sit
     puts "The Dog is sitting"
   end
   
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> c3506df4de7da6863fa144b632a6cdb54b2a15b8
